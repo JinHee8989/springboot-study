96a2dd4417b03e2a3469c0bd40e8cd3c6b38528d
jinheebyeon <wlsgml8989@gmail.com>
2021-06-25 00:00:30 +0900

    테스트 코드 추가

--- server/basic-test/src/test/java/com/study/springbootsecurity/Test1.java
+++ server/basic-test/src/test/java/com/study/springbootsecurity/Test1.java
@@ -1,4 +1,15 @@
 package com.study.springbootsecurity;
 
+import org.junit.jupiter.api.DisplayName;
+import org.junit.jupiter.api.Test;
+import static org.junit.jupiter.api.Assertions.assertEquals;
+
 public class Test1 {
+
+    @DisplayName("1.테스트")
+    @Test
+    void test_1(){
+        assertEquals("test","test");
+    }
+
 }
