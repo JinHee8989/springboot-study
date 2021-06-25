5955164b82fdb8d00cf5c866815fe81fdc4eb458
jinheebyeon <wlsgml8989@gmail.com>
2021-06-25 23:49:45 +0900

    타모듈 클래스 레퍼런스 추가

--- server/basic-test/src/main/java/com/study/springbootsecurity/BasicTestApplication.java
+++ server/basic-test/src/main/java/com/study/springbootsecurity/BasicTestApplication.java
@@ -6,6 +6,7 @@ import org.springframework.boot.autoconfigure.SpringBootApplication;
 @SpringBootApplication
 public class BasicTestApplication {
     public static void main(String[] args){
+        Person person = null;    //다른 모듈의 클래스를 레퍼런스하기 (build.gradle에서 해당 모듈을 컴파일해야함)
         SpringApplication.run(BasicTestApplication.class, args);
 
     }
