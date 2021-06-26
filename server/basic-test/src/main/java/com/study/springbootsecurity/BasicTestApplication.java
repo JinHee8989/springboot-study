0584ba4e00306058750330f2503c903982ce63a2
jinheebyeon <wlsgml8989@gmail.com>
2021-06-26 23:06:50 +0900

    gradle exception 처리중

--- server/basic-test/src/main/java/com/study/springbootsecurity/BasicTestApplication.java
+++ server/basic-test/src/main/java/com/study/springbootsecurity/BasicTestApplication.java
@@ -6,7 +6,7 @@ import org.springframework.boot.autoconfigure.SpringBootApplication;
 @SpringBootApplication
 public class BasicTestApplication {
     public static void main(String[] args){
-        Person person = null;    //다른 모듈의 클래스를 레퍼런스하기 (build.gradle에서 해당 모듈을 컴파일해야함)
+        Person person = Person.builder().name("test").build();    //다른 모듈의 클래스를 레퍼런스하기 (build.gradle에서 해당 모듈을 컴파일해야함)
         SpringApplication.run(BasicTestApplication.class, args);
 
     }
