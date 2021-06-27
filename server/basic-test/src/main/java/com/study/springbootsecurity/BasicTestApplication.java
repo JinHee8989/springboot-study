39c7dca60d8e553b3677e287d88260eb28a2b0cf
jinheebyeon <wlsgml8989@gmail.com>
2021-06-27 17:24:38 +0900

    README.md 파일 추가

--- server/basic-test/src/main/java/com/study/springbootsecurity/BasicTestApplication.java
+++ server/basic-test/src/main/java/com/study/springbootsecurity/BasicTestApplication.java
@@ -7,7 +7,8 @@ import org.springframework.boot.autoconfigure.SpringBootApplication;
 public class BasicTestApplication {
     public static void main(String[] args){
         Person person = Person.builder().name("test").build();    //다른 모듈의 클래스를 레퍼런스하기 (build.gradle에서 해당 모듈을 컴파일해야함)
-        SpringApplication.run(BasicTestApplication.class, args);
+        System.out.println("person = " + person);
+//        SpringApplication.run(BasicTestApplication.class, args);
 
     }
 }
