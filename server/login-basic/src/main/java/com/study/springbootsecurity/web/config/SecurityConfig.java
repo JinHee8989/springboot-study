4706e50b89415df651e767d24c182907fe4638a7
jinheebyeon <wlsgml8989@gmail.com>
2021-07-01 23:08:47 +0900

    AuthenticationDetails 커스터마이징 코드 추가

--- server/login-basic/src/main/java/com/study/springbootsecurity/web/config/SecurityConfig.java
+++ server/login-basic/src/main/java/com/study/springbootsecurity/web/config/SecurityConfig.java
@@ -1,19 +1,27 @@
 package com.study.springbootsecurity.web.config;
 
-import org.apache.catalina.filters.CsrfPreventionFilter;
 import org.springframework.boot.autoconfigure.security.servlet.PathRequest;
+import org.springframework.context.annotation.Bean;
+import org.springframework.security.access.hierarchicalroles.RoleHierarchy;
+import org.springframework.security.access.hierarchicalroles.RoleHierarchyImpl;
 import org.springframework.security.config.annotation.authentication.builders.AuthenticationManagerBuilder;
+import org.springframework.security.config.annotation.method.configuration.EnableGlobalMethodSecurity;
 import org.springframework.security.config.annotation.web.builders.HttpSecurity;
 import org.springframework.security.config.annotation.web.builders.WebSecurity;
 import org.springframework.security.config.annotation.web.configuration.EnableWebSecurity;
 import org.springframework.security.config.annotation.web.configuration.WebSecurityConfigurerAdapter;
 import org.springframework.security.core.userdetails.User;
-import org.springframework.security.web.authentication.UsernamePasswordAuthenticationFilter;
 
 @EnableWebSecurity(debug = true)
+@EnableGlobalMethodSecurity(prePostEnabled = true)  //MethodSecurity는 우리가 기존에 사용했던 SecurityConfig 설정이 적용되지 않음. 이걸 해줘야 타임리프에서 적용한 security에도 적용됨
 public class SecurityConfig extends WebSecurityConfigurerAdapter {
 
-    UsernamePasswordAuthenticationFilter filter;
+    private final CustomAuthDetails customAuthDetails;  //의존성 주입
+
+    public SecurityConfig(CustomAuthDetails customAuthDetails) {
+        this.customAuthDetails = customAuthDetails;
+    }
+
 
     @Override
     protected void configure(AuthenticationManagerBuilder auth) throws Exception {  //이렇게 설정해주면 application.yml에 설정해놓은 user는 무시하게됨(로그인 불가)
@@ -42,10 +50,13 @@ public class SecurityConfig extends WebSecurityConfigurerAdapter {
                         .permitAll()                        //그래서 permitAll()을 무한루프에 안 빠지고 로그인페이지로 접근 가능
                         .defaultSuccessUrl("/",false)   //로그인 후 이동할 페이지 설정(alwaysUse가 false면 로그인 전 액세스한 페이지로 리디렉션, true면 무조건 지정한 페이지로 이동)
                         .failureUrl("/login-error") //로그인 실패했을 때 이동할 페이지 설정
+                        .authenticationDetailsSource(customAuthDetails) //userDetails를 customize할 필요가 있을때 설정
+
                 )
                 .logout(
                         logout->logout.logoutSuccessUrl("/")    //로그아웃 후 이동할 페이지 설정
                 )
+                .exceptionHandling(exception -> exception.accessDeniedPage("/access-denied"))   //예외 발생시 이동할 페이지 설정
         ;
     }
 
@@ -55,5 +66,13 @@ public class SecurityConfig extends WebSecurityConfigurerAdapter {
                 .requestMatchers(
                         PathRequest.toStaticResources().atCommonLocations()     //리소스의 static을 몽땅 웹리소스로 잡아주고있음
                 );
+    }
+
+
+    @Bean   //(A라는 권한이 B의 권한도 가질수 있도록 설정
+    RoleHierarchy roleHierarchy(){
+        RoleHierarchyImpl roleHierarchy = new RoleHierarchyImpl();
+        roleHierarchy.setHierarchy("ROLE_ADMIN > ROLE_USER");
+        return roleHierarchy;
     }
 }
--- server/login-basic/src/main/java/com/study/springbootsecurity/web/controller/HomeController.java
+++ server/login-basic/src/main/java/com/study/springbootsecurity/web/controller/HomeController.java
@@ -2,12 +2,21 @@ package com.study.springbootsecurity.web.controller;
 
 
 import org.springframework.security.access.prepost.PreAuthorize;
+import org.springframework.security.core.Authentication;
+import org.springframework.security.core.context.SecurityContextHolder;
 import org.springframework.stereotype.Controller;
 import org.springframework.ui.Model;
 import org.springframework.web.bind.annotation.GetMapping;
+import org.springframework.web.bind.annotation.ResponseBody;
 
 @Controller
 public class HomeController {
+
+    @ResponseBody   //이걸 설정해줘야 JSON 객체로 전달됨
+    @GetMapping("/auth")
+    public Authentication auth(){
+        return SecurityContextHolder.getContext().getAuthentication();  //Authentication 정보를 볼 수 있음
+    }
 
     @GetMapping("/")
     public String main(){
@@ -18,6 +27,8 @@ public class HomeController {
     public String login(){
         return "loginForm";
     }
+
+
 
     @GetMapping("/login-error")
     public String loginError(Model model){
@@ -41,5 +52,7 @@ public class HomeController {
     public String adminPage(){
         return "AdminPage";
     }
+
+
 }
 
