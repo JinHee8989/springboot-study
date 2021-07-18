f10cddf681ccb8c887714ad3f3c8509d0889cb83
jinheebyeon <wlsgml8989@gmail.com>
2021-07-18 23:32:45 +0900

    authenticationProvider 커스터마이징

--- server/login-customer-filter/src/main/java/com/study/springbootsecurity/web/config/SecurityConfig.java
+++ server/login-customer-filter/src/main/java/com/study/springbootsecurity/web/config/SecurityConfig.java
@@ -1,4 +1,23 @@
 package com.study.springbootsecurity.web.config;
 
-public class SecurityConfig {
+import com.study.springbootsecurity.web.student.StudentManager;
+import org.springframework.security.config.annotation.authentication.builders.AuthenticationManagerBuilder;
+import org.springframework.security.config.annotation.web.configuration.EnableWebSecurity;
+import org.springframework.security.config.annotation.web.configuration.WebSecurityConfigurerAdapter;
+
+@EnableWebSecurity(debug = true)
+public class SecurityConfig extends WebSecurityConfigurerAdapter {
+
+    private final StudentManager studentManager;
+
+    public SecurityConfig(StudentManager studentManager) {
+        this.studentManager = studentManager;
+    }
+
+    @Override
+    protected void configure(AuthenticationManagerBuilder auth){
+        auth.authenticationProvider(studentManager);    //studentManager가 provider가 됨
+    }
+
+
 }
